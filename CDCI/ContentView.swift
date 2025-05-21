94dba0cccce2a33c407ae824aeb4ea097f6ec8c6
Tiziano Cialfi <tiziano.cialfi@soft.it>
2025-05-21 15:38:54 +0200

    Add swiftlint to CI

--- CDCI/ContentView.swift
+++ CDCI/ContentView.swift
@@ -8,12 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State private var value: Double = 0
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            .gesture(
+              DragGesture()
+                .onChanged({ value in
+                  self.value = value.translation.width
+                })
+            )
         }
         .padding()
     }
