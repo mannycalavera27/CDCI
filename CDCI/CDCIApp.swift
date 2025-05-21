266f9bec05d9eb3d42c335f6d813abaa6246757f
Tiziano Cialfi <tiziano.cialfi@soft.it>
2025-05-21 09:12:28 +0200

    Initial Commit

---
+++ CDCI/CDCIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CDCIApp.swift
+//  CDCI
+//
+//  Created by Tiziano Cialfi on 21/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CDCIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CDCITests/CDCITests.swift
@@ -0,0 +1,17 @@
+//
+//  CDCITests.swift
+//  CDCITests
+//
+//  Created by Tiziano Cialfi on 21/05/25.
+//
+
+import Testing
+@testable import CDCI
+
+struct CDCITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
